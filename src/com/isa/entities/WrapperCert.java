fd736c827738a9ad46fb5355dec84b0502c1bf9b
jmg216 <juanma.miraballes@gmail.com>
2016-01-19 10:38:21 -0300

    Archivos faltantes.

---
+++ src/com/isa/entities/WrapperCert.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.isa.entities;
+
+/**
+ *
+ * @author JMiraballes
+ */
+public class WrapperCert {
+   
+    private Codigo codigo;
+    private String msj;
+
+    public Codigo getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Codigo codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMsj() {
+        return msj;
+    }
+
+    public void setMsj(String msj) {
+        this.msj = msj;
+    }
+    
+    
+}
